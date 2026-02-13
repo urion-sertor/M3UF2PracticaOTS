9a18ca73f452e720eb9eb69cbaf1a347468e72d5
Oriol Torres Sanchez <orioltorsan@campus.monlau.com>
2026-02-13 13:24:04 +0100

    0.1

---
+++ src/view/console/Main.java
@@ -0,0 +1,14 @@
+package view.console;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author orioltorsan
+ */
+public class Main {
+    
+}
