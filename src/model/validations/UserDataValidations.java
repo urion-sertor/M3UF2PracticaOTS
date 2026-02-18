a7d1a35b8eaa9440e1dc4b1072fd09a1107cf235
Oriol Torres Sanchez <orioltorsan@campus.monlau.com>
2026-02-18 13:28:14 +0100

    message

--- src/model/validations/UserDataValidations.java
+++ src/model/validations/UserDataValidations.java
@@ -8,6 +8,16 @@ package model.validations;
  *
  * @author orioltorsan
  */
+
+// 1.	boolean checkId (int typeDoc, String id): Debe validar si el NIF (tipo de documento 1) recibido es correcto o no. Si es correcto retornará true, en caso contrario false. De momento si el tipo de documento es otro se devolverá false.
+// 2.	boolean checkFormatDate(String date): Debe validar que el formato de la fecha y su valor es correcto. El formato esperado es “dd/mm”aaaa”. Si es correcto retornará true, en caso contrario false. Usa la función split de la clase String.
+// 3.	int calculateAge(String birthDate): Debe calcular la edad en base a la fecha de nacimiento. Previamente debe validarse que la fecha sea correcta. En caso de que la fecha sea coherente se devolverá la edad calculada, en caso contrario devolveremos -1.
+// 4.	boolean checkPostalCode(String zip): Debe validar que el formato del código postal sea correcto. El formato esperado es nnnnn (5 números). Si es correcto retornará true, en caso contrario false. 
+// 5.	boolean isNumeric(String str): Debe Validar que el contenido del String es numérico. Si lo es retornará true, en caso contrario false. 
+// 6.	boolean isAlphabetic(String str): Debe Validar que el contenido del String solo tenga letras. Si es así retornará true, en caso contrario false. 
+// 7.	boolean checkEmail(String email): Debe validar que el formato del correo se corresponde con un correo electronico (debe contener una @ y la terminación debe ser .com, .es, …). Si es correcto retornará true, en caso contrario false. 
+// 8.	boolean checkName(String name): Debe validar que el nombre tiene una longitud lógica y que no contenga números. Si es correcto retornará true, en caso contrario false. 
+
 public class UserDataValidations {
     
 }
