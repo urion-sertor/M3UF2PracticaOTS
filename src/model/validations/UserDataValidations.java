17a1d5afd867788e1e5904ae32a6a1883fe8fb40
Oriol Torres Sanchez <orioltorsan@campus.monlau.com>
2026-02-19 10:04:10 +0100

    message

--- src/model/validations/UserDataValidations.java
+++ src/model/validations/UserDataValidations.java
@@ -4,20 +4,153 @@
  */
 package model.validations;
 
+import java.util.Scanner;
+
 /**
  *
  * @author orioltorsan
  */
 
-// 1.	boolean checkId (int typeDoc, String id): Debe validar si el NIF (tipo de documento 1) recibido es correcto o no. Si es correcto retornará true, en caso contrario false. De momento si el tipo de documento es otro se devolverá false.
-// 2.	boolean checkFormatDate(String date): Debe validar que el formato de la fecha y su valor es correcto. El formato esperado es “dd/mm”aaaa”. Si es correcto retornará true, en caso contrario false. Usa la función split de la clase String.
-// 3.	int calculateAge(String birthDate): Debe calcular la edad en base a la fecha de nacimiento. Previamente debe validarse que la fecha sea correcta. En caso de que la fecha sea coherente se devolverá la edad calculada, en caso contrario devolveremos -1.
-// 4.	boolean checkPostalCode(String zip): Debe validar que el formato del código postal sea correcto. El formato esperado es nnnnn (5 números). Si es correcto retornará true, en caso contrario false. 
-// 5.	boolean isNumeric(String str): Debe Validar que el contenido del String es numérico. Si lo es retornará true, en caso contrario false. 
-// 6.	boolean isAlphabetic(String str): Debe Validar que el contenido del String solo tenga letras. Si es así retornará true, en caso contrario false. 
-// 7.	boolean checkEmail(String email): Debe validar que el formato del correo se corresponde con un correo electronico (debe contener una @ y la terminación debe ser .com, .es, …). Si es correcto retornará true, en caso contrario false. 
-// 8.	boolean checkName(String name): Debe validar que el nombre tiene una longitud lógica y que no contenga números. Si es correcto retornará true, en caso contrario false. 
-
 public class UserDataValidations {
+
+    // 1. boolean checkId (int typeDoc, String id): Debe validar si el NIF (tipo de documento 1) recibido es correcto o no. Si es correcto retornará true, en caso contrario false. De momento si el tipo de documento es otro se devolverá false.
+    public boolean checkId(int typeDoc, String id) {
+        if (typeDoc != 1) {
+            return false;
+        }
+        
+        if (id.length() != 9) {
+            return false;
+        }
+        
+        String numbers = id.substring(0, 8);
+        char letter = id.charAt(8);
+        
+        if (!isNumeric(numbers) || !isAlphabetic(String.valueOf(letter))) {
+            return false;
+        }
+        
+        return true;
+    }
+
+    // 2. boolean checkFormatDate(String date): Debe validar que el formato de la fecha y su valor es correcto. El formato esperado es “dd/mm”aaaa”. Si es correcto retornará true, en caso contrario false. Usa la función split de la clase String.
+    public boolean checkFormatDate(String date) {
+        String[] ddmmyy = date.split("/");
+
+        if (ddmmyy.length != 3) {
+            return false;
+        }
+
+        if (!isNumeric(ddmmyy[0]) || !isNumeric(ddmmyy[1]) || !isNumeric(ddmmyy[2])) {
+            return false;
+        }
+
+        int day = Integer.parseInt(ddmmyy[0]);
+        int month = Integer.parseInt(ddmmyy[1]);
+        int year = Integer.parseInt(ddmmyy[2]);
+
+        if (day < 1 || day > 31) {
+            return false;
+        }
+        if (month < 1 || month > 12) {
+            return false;
+        }
+        if (year < 1000 || year > 9999) {
+            return false;
+        }
+
+        return true;
+    }
+
+    // 3. int calculateAge(String birthDate): Debe calcular la edad en base a la fecha de nacimiento. Previamente debe validarse que la fecha sea correcta. En caso de que la fecha sea coherente se devolverá la edad calculada, en caso contrario devolveremos -1.
+    public int calculateAge(String birthDate) {
+        if (checkFormatDate(birthDate) == false) {
+            return -1;
+        }
+
+        String[] ddmmyy = birthDate.split("/");
+        int yearBirth = Integer.parseInt(ddmmyy[2]);
+
+        int yearCurrent = 2026;
+        int age = yearCurrent - yearBirth;
+
+        if (age < 0 || age > 120) {
+            return -1;
+        }
+
+        return age;
+    }
+
+    // 4. boolean checkPostalCode(String zip): Debe validar que el formato del código postal sea correcto. El formato esperado es nnnnn (5 números). Si es correcto retornará true, en caso contrario false. 
+    public boolean checkPostalCode(String zip) {
+        if (zip.length() != 5) {
+            return false;
+        }
+
+        if (!isNumeric(zip)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    // 5. boolean isNumeric(String str): Debe Validar que el contenido del String es numérico. Si lo es retornará true, en caso contrario false. 
+    public boolean isNumeric(String str) {
+        if (str == null || str.equals("")) {
+            return false;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+
+            if (c < '0' || c > '9') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    // 6. boolean isAlphabetic(String str): Debe Validar que el contenido del String solo tenga letras. Si es así retornará true, en caso contrario false. 
+    public boolean isAlphabetic(String str) {
+        if (str == null || str.equals("")) {
+            return false;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+
+            if (!((c <= 'a' && c >= 'z') || (c <= 'A' && c >= 'Z') || c == ' ')) {
+                return false;
+            }
+        }
+
+        return true;
+    }
     
+    // 7. boolean checkEmail(String email): Debe validar que el formato del correo se corresponde con un correo electronico (debe contener una @ y la terminación debe ser .com, .es, …). Si es correcto retornará true, en caso contrario false. 
+    public boolean checkEmail (String email) {
+        if (!email.contains("@")) {
+            return false;
+        }
+        
+        if (!email.endsWith(".com") && !email.endsWith(".es") && !email.endsWith(".org") && !email.endsWith(".net")) {
+            return false;
+        }
+        
+        return true;
+    }
+    
+    // 8. boolean checkName(String name): Debe validar que el nombre tiene una longitud lógica y que no contenga números. Si es correcto retornará true, en caso contrario false. 
+    public boolean checkName(String name) {
+        if (!isAlphabetic(name)) {
+            return false;
+        }
+        
+        if (name.length() < 2 || name.length() > 50) {
+            return false;
+        }
+        
+        return true;
+    }
 }
--- src/view/console/Main.java
+++ src/view/console/Main.java
@@ -1,14 +1,69 @@
 package view.console;
 
+import java.util.Scanner;
+import model.validations.UserDataValidations;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author orioltorsan
  */
 public class Main {
-    
+
+    static Scanner sc = new Scanner(System.in);
+    static UserDataValidations uv = new UserDataValidations();
+
+    public static void main(String[] args) {
+        menu();
+    }
+
+    public static void menu() {
+        System.out.println("Menu: ");
+        System.out.println("1 - Check ID");
+        System.out.println("2 - Check Format Date");
+        System.out.println("3 - Calculate Age");
+        System.out.println("4 - Check Postal Code");
+        System.out.println("5 - Is Numeric");
+        System.out.println("6 - Is Alphabetic");
+        System.out.println("7 - Check Email");
+        System.out.println("8 - Check Name");
+        System.out.print("Enter an option: ");
+
+        int option = sc.nextInt();
+        sc.nextLine();
+
+        switch (option) {
+            case 1:
+                System.out.println("");
+                break;
+            case 2:
+                System.out.println("");
+                break;
+            case 3:
+                System.out.println("");
+                break;
+            case 4:
+                System.out.println("");
+                break;
+            case 5:
+                System.out.println("");
+                break;
+            case 6:
+                System.out.println("");
+                break;
+            case 7:
+                System.out.println("");
+                break;
+            case 8:
+                System.out.println("");
+                break;
+            default:
+                System.out.println("Invalid option.");
+                break;
+        }
+    }
+
 }
